bc028031e2c1c0af90abdacad4797b7f601b8f44
grimmauld <soeren@benjos.de>
2021-06-15 19:07:10 +0200

    Try to fix broken bot reload

--- src/main/java/mod/grimmauld/discordchat/DiscordChat.java
+++ src/main/java/mod/grimmauld/discordchat/DiscordChat.java
@@ -16,7 +16,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(DiscordChat.MODID)
 public class DiscordChat {
 	public static final String MODID = "discordchat";
-	public static final String VERSION = "0.0.3";
+	public static final String VERSION = "0.0.6";
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 	public static final DiscordBotContainer BOT_INSTANCE = new DiscordBotContainer();
 	public static final LazyOptionalContainer<MinecraftServer> SERVER_INSTANCE = new LazyOptionalContainer<>();
--- src/main/java/mod/grimmauld/discordchat/util/DiscordBotContainer.java
+++ src/main/java/mod/grimmauld/discordchat/util/DiscordBotContainer.java
@@ -5,6 +5,7 @@ import mod.grimmauld.discordchat.EventListener;
 import net.dv8tion.jda.api.JDA;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class DiscordBotContainer extends LazyOptionalContainer<DiscordBot> {
@@ -20,7 +21,20 @@ public class DiscordBotContainer extends LazyOptionalContainer<DiscordBot> {
 		}).orElseGet(Optional::empty);
 	}
 
+	public void ifJDAPresent(Consumer<JDA> action) {
+		ifPresent(bot -> {
+			if (bot == null || bot.jda == null)
+				return;
+			JDA.Status status = bot.jda.getStatus();
+			if (status == JDA.Status.SHUTDOWN || status == JDA.Status.SHUTTING_DOWN)
+				return;
+			action.accept(bot.jda);
+		});
+	}
+
+
 	public int relaunchBot() {
+		ifJDAPresent(JDA::shutdownNow);
 		connect(DiscordBot::new);
 		EventListener.resetSyncCycle();
 		return 1;
